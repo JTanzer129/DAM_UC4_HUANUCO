7e6f114ec1c513a67f12ba78cda0b12af5c808e1
JTanzer129 <jamesjustdance2019@gmail.com>
2023-07-10 15:21:24 -0500

    Notas en Columnas

---
+++ app/src/main/java/com/example/dam_uc4_huanuco/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.dam_uc4_huanuco;
+
+public interface NotasInteractionListener {
+    void editNotaClick (Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
